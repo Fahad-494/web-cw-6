ed0fbf2923764987a7d8abc58553d813e290f23c
Fahad-494 <66741156+e2id@users.noreply.github.com>
2020-07-01 21:09:19 +0300

    مو كامل

---
+++ scripts.js
@@ -0,0 +1,23 @@
+//logger
+function logger(array){
+    
+    for (let fahad of array ) {
+        console.log(fahad);
+    }
+
+}
+let friend =["Nasser","Hamad","Fuhaid"]
+logger(friend);
+
+
+let array2 = ["18","60","45"]
+
+function hottestday (hot,number){
+    for(weather of array2){
+       if (letweather > number )
+       console.log(weather)
+
+    }
+}    
+
+hottestday(array2 , 2)
